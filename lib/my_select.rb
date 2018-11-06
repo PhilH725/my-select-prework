1e2100b0cf0ae874cc4d3255dc81003121768c54
Phil Hartley <philh725@gmail.com>
2018-11-06 01:04:11 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,9 +3,10 @@ def my_select(collection)
   i = 0
   new_array = []
   while i < collection.size
-    if yield collection[i] == True
+    if yield collection[i]
       new_array.push(collection[i])
     end
     i += 1
   end
+  new_array
 end
